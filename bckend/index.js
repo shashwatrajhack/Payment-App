058e935efeec7032e0bb247aa5ef24fbc37a7b32
shashwatrajhack <er.shashwatraj@gmail.com>
2025-03-19 14:17:02 +0530

    basic structure and validation using zod

--- bckend/index.js
+++ bckend/index.js
@@ -1,2 +1,18 @@
 const express = require('express');
+const zod = require('zod');
+const {User} = require('./db');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('./config');
+const rootRouter = require('./routes/index');
+app.use(cors());
+//app.use(bodyparser.json());
+app.use(express.json());
+
+
+
 const app = express();
+
+app.use("/api/v1",rootRouter);
+
+app.listen(3000);
+
